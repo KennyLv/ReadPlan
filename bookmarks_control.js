98b49a4e321b108ade141ce819895e4a7c2b53ba
Qing, Lv <luqin@airbiquity.com>
2015-04-29 15:38:16 +0800

    update bookmark

---
+++ bookmarks_control.js
@@ -0,0 +1,12 @@
+/*
+<link rel="stylesheet" href="bookmarks_style.css" type="text/css"/>
+<script  src="bookmarks_control.js" type="text/javascript" ></script>
+*/
+
+var dt = document.getElementsByTagName('dt');
+for(var i=0;i<dt.length;i++){
+				var dddd = dt[i];
+				if(dddd.childNodes[0].tagName == "H3" && dddd.childNodes[1]){
+								dddd.childNodes[0].style.display = "none";
+				}
+}
